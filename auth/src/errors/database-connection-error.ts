6d2129afb1cb6a8b294d3caa61d32bed614f156f
guille garcia <ggarciacardiel@gmail.com>
2024-09-01 18:00:36 +0200

    serializeErrors to move logic into errors

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,9 +1,14 @@
 export class DatabaseConnectionError extends Error {
   reason = "Error connecting to database";
-
+  statusCode = 500;
+  
   constructor() {
     super();
 
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
   }
 }
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -3,24 +3,20 @@ import { RequestValidationError } from '../errors/request-validation-error';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (
-  err: Error,
-  req: Request,
-  res: Response,
+  err: Error, 
+  req: Request, 
+  res: Response, 
   next: NextFunction
 ) => {
   if (err instanceof RequestValidationError) {
-    const formattedErrors = err.errors.map((error) => {
-      if (error.type === 'field') {
-        return { message: error.msg, field: error.path };
-      }
-    });
-    return res.status(400).send({ errors: formattedErrors });
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
+
   if (err instanceof DatabaseConnectionError) {
-    return res.status(500).send({ errors: [{ message: err.reason }] });
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   res.status(400).send({
-    errors: [{ message: 'Something went wrong' }],
+    message: err.message
   });
 };
