c23bf059ad33a75b5a4e9fb6a18831ddef5d659a
guille garcia <ggarciacardiel@gmail.com>
2024-09-01 15:36:16 +0200

    converts errors to responses

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,10 +1,9 @@
 export class DatabaseConnectionError extends Error {
-  reason = 'Error connecting to database'; // Fixed
+  reason = "Error connecting to database";
 
   constructor() {
     super();
 
-    // Only because we are extending a built in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
   }
 }
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,14 +1,26 @@
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (
-  err: Error, 
-  req: Request, 
-  res: Response, 
+  err: Error,
+  req: Request,
+  res: Response,
   next: NextFunction
 ) => {
-  console.log('Something went wrong', err);
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map((error) => {
+      if (error.type === 'field') {
+        return { message: error.msg, field: error.path };
+      }
+    });
+    return res.status(400).send({ errors: formattedErrors });
+  }
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({ errors: [{ message: err.reason }] });
+  }
 
   res.status(400).send({
-    message: err.message
+    errors: [{ message: 'Something went wrong' }],
   });
 };
