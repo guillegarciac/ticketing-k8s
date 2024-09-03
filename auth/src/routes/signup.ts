6018269cd8ec8838a43a4742621191d91b597e56
guille garcia <ggarciacardiel@gmail.com>
2024-09-03 19:03:15 +0200

    creates jwt secret and adds env to auth-depl

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -39,7 +39,7 @@ async (req: Request, res: Response) => {
   const userJwt = jwt.sign({
     id: user.id,
     email: user.email
-  }, 'jwetkey');
+  }, process.env.JWT_KEY!);
 
   // Store it on session object
   req.session = {
