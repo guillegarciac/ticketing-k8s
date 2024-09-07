bb4b18d7e587df55c0c4acdde89258fe5bfbb61e
guille garcia <ggarciacardiel@gmail.com>
2024-09-07 13:52:02 +0200

    creates a few more signup tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,39 @@ it('returns a 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'p',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'valid@valid.copm',
+    }) // empty object
+    .expect(400);
+
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'password',
+    }) // empty object
+    .expect(400);  
+});
