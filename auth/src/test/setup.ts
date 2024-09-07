811c020a7b24f6fb8a5e81223b553b60f516207e
guille garcia <ggarciacardiel@gmail.com>
2024-09-07 14:29:31 +0200

    creates auth helper function

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,11 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 beforeAll(async () => {
@@ -21,3 +26,17 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+global.signin = async (): Promise<string[]> => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+  return cookie as string[];
+}
+
