054e77274a2d9d2e31ed9460fba49e32b7a1da7c
guille garcia <ggarciacardiel@gmail.com>
2024-09-08 13:37:50 +0200

    creates ingree-nginx fucntion to pass on initialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,33 @@
-const LandingPage = ({ color }) => {
-  console.log('Im on Client', color);
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser');
+  console.log(currentUser);
+
   return <h1>Landing Page</h1>;
-}
+};
 
-// This function will be executed on the server side
-// This function will not be executed on the client side
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    // we are on the server!
+    // requests should be made to http://ingress-nginx.ingress-nginx...laksdjfk
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
 
-LandingPage.getInitialProps = async (context) => {
-  console.log('Im on Server');
-  return { color: 'red' };
-}
+    return data;
+  } else {
+    // we are on the browser!
+    // requests can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
+  }
+  return {};
+};
 
 export default LandingPage;
-
