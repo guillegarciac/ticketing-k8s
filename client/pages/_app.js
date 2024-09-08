295160acb8b5efd7bfc62d12cdf1181f9ab34a02
guille garcia <ggarciacardiel@gmail.com>
2024-09-08 14:03:54 +0200

    updates _app.js to pass props through and be able to manage multiple getIntitialProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,32 @@
-import 'bootstrap/dist/css/bootstrap.css';
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
 // This is the root component for all pages in the app.
 // It's used to keep state when navigating between pages.
 // It's also used to add global CSS styles.
+
+import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
+
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>Header! {currentUser.email}</h1>
+      <Component {...pageProps} />;
+    </div>
+  );
+}
+
+AppComponent.getInitialProps = async appContext => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    ...data
+  };
+}
+
+export default AppComponent;
