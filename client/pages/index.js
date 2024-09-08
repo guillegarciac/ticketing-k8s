c39142a08f378f8eb9290ecc3937be9d7c43ae49
guille garcia <ggarciacardiel@gmail.com>
2024-09-08 13:01:25 +0200

    updates index.js to understand server side rendering with getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,15 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to the home page!</p>
-    </div>
-  );
+const LandingPage = ({ color }) => {
+  console.log('Im on Client', color);
+  return <h1>Landing Page</h1>;
 }
+
+// This function will be executed on the server side
+// This function will not be executed on the client side
+
+LandingPage.getInitialProps = async (context) => {
+  console.log('Im on Server');
+  return { color: 'red' };
+}
+
+export default LandingPage;
+
