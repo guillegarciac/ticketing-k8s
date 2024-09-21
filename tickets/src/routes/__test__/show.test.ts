5255e2684a53681cbbc6c3f7ced43309bf0e589e
guille garcia <ggarciacardiel@gmail.com>
2024-09-21 12:38:16 +0200

    updates show test to include mongoId on validate

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,22 +1,19 @@
-import request from 'supertest';
-import { app } from '../../app';
+import request from "supertest";
+import { app } from "../../app";
+import mongoose from "mongoose";
 
-it('returns a 404 if the ticket is not found', async () => {
-  const response = await request(app)
-    .get('/api/tickets/asdfasdf')
-    .send()
+it("returns a 404 if the ticket is not found", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app).get(`/api/tickets/${id}`).send().expect(404);
+});
 
-    console.log(response.body);
-}
-);
-
-it('returns the ticket if the ticket is found', async () => {
-  const title = 'concert';
+it("returns the ticket if the ticket is found", async () => {
+  const title = "concert";
   const price = 20;
 
   const response = await request(app)
-    .post('/api/tickets')
-    .set('Cookie', global.signin())
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
     .send({
       title,
       price,
@@ -30,5 +27,4 @@ it('returns the ticket if the ticket is found', async () => {
 
   expect(ticketResponse.body.title).toEqual(title);
   expect(ticketResponse.body.price).toEqual(price);
-}
-);
+});
