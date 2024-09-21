232515f6fe0e2ad5a008bcd16fc37100504a8e02
guille garcia <ggarciacardiel@gmail.com>
2024-09-21 14:35:27 +0200

    updates listener

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,18 @@
+import nats from 'node-nats-streaming';
+
+const stan = nats.connect('ticketing', '123', {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+  console.log('Listener connected to NATS');
+
+  const subscription = stan.subscribe('ticket:created');
+  subscription.on('message', (msg) => {
+    const data = msg.getData();
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+  });
+});
+
