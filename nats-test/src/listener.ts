6d8c0c2399af108270acb4726ddd0e8184ea1681
guille garcia <ggarciacardiel@gmail.com>
2024-09-30 16:19:42 +0200

    refactor event exports

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,7 +1,7 @@
 // Import the necessary modules from node-nats-streaming and crypto
-import nats, { Message, Stan } from "node-nats-streaming";
+import nats from "node-nats-streaming";
 import { randomBytes } from "crypto";
-import { subscribe } from "diagnostics_channel";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 
 // Clear the console output to ensure a clean slate for logging
 console.clear();
@@ -33,59 +33,6 @@ stan.on("connect", () => {
 process.on("SIGINT", () => stan.close());
 process.on("SIGTERM", () => stan.close());
 
-abstract class Listener {
-  abstract subject: string;
-  abstract queueGroupName: string;
-  abstract onMessage(data: any, msg: Message): void;
-  private client: Stan;
-  protected ackWait = 5 * 1000;
-
-  constructor(client: Stan) {
-    this.client = client;
-  }
-
-  subscriptionOptions() {
-    return this.client
-      .subscriptionOptions()
-      .setDeliverAllAvailable()
-      .setManualAckMode(true)
-      .setAckWait(this.ackWait)
-      .setDurableName(this.queueGroupName);
-  }
-
-  listen() {
-    const subscription = this.client.subscribe(
-      this.subject,
-      this.queueGroupName,
-      this.subscriptionOptions()
-    );
-
-    subscription.on("message", (msg: Message) => {
-      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
-
-      const parsedData = this.parseMessage(msg);
-      this.onMessage(parsedData, msg);
-    });
-  }
-
-  parseMessage(msg: Message) {
-    const data = msg.getData();
-    return typeof data === "string"
-      ? JSON.parse(data)
-      : JSON.parse(data.toString("utf8"));
-  }
-}
-
-class TicketCreatedListener extends Listener {
-  subject = "ticket:created";
-  queueGroupName = "payments-service";
-
-  onMessage(data: any, msg: Message) {
-    console.log("Event data!", data);
-
-    msg.ack();
-  }
-}
 
 // to run this we need to user kubectl port-forward nats-depl-67687c6fbd-l6k8c 4222:4222
 // then run npm run listen
