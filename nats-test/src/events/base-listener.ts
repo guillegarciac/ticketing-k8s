c34679c4cbe750fdfcfeb9c4d64924f6f39c48af
guille garcia <ggarciacardiel@gmail.com>
2024-09-30 16:33:14 +0200

    enforcing listener subjects for Ts

--- nats-test/src/events/base-listener.ts
+++ nats-test/src/events/base-listener.ts
@@ -1,10 +1,16 @@
 import { Stan, Message } from "node-nats-streaming";
+import { Subjects } from "./subjects";
+
+interface Event {
+  subject: Subjects;
+  data: any;
+}
 
 
-export abstract class Listener {
-  abstract subject: string;
+export abstract class Listener<T extends Event> {
+  abstract subject: T['subject'];
   abstract queueGroupName: string;
-  abstract onMessage(data: any, msg: Message): void;
+  abstract onMessage(data: T['data'], msg: Message): void;
   private client: Stan;
   protected ackWait = 5 * 1000;
 
