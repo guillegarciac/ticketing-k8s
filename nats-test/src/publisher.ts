fec561d5de267737c036f05aa1cd103c16c8f639
guille garcia <ggarciacardiel@gmail.com>
2024-10-01 08:17:27 +0200

    updates common module and ggctickets/common

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,19 +1,23 @@
-import nats from 'node-nats-streaming';
-import { TicketCreatedPublisher } from './events/ticket-created-publisher';
+import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
-const stan = nats.connect('ticketing', 'abc', {
-  url: 'http://localhost:4222',
+const stan = nats.connect("ticketing", "abc", {
+  url: "http://localhost:4222",
 });
 
-stan.on('connect', () => {
-  console.log('Publisher connected to NATS');
+stan.on("connect", async () => {
+  console.log("Publisher connected to NATS");
 
   const publisher = new TicketCreatedPublisher(stan);
-  publisher.publish({
-    id: '123',
-    title: 'concert',
-    price: 20,
-  });
+  try {
+    await publisher.publish({
+      id: "123",
+      title: "concert",
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 
   /* const data = JSON.stringify({
     id: '123',
