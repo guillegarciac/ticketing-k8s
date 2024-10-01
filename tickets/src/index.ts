fe93e839d26dcc4e2a84d86af5e8bf6b4c19efe1
guille garcia <ggarciacardiel@gmail.com>
2024-10-01 10:55:50 +0200

    adds shutdown signal to nats-wrapper

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -13,6 +13,15 @@ const start = async () => {
   
   try {
     await natsWrapper.connect("ticketing", "abc", "http://nats-srv:4222");
+    // Event listener for when the connection is closed, logging a message and exiting the process
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI, {
     });
     console.log("Connected to MongoDb");
