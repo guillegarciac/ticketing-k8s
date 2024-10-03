500c6e77d45860a85fe0fc8c8d5fee1040775900
guille garcia <ggarciacardiel@gmail.com>
2024-10-03 18:37:03 +0200

    Creates NATS Env variables in tickets-depl.yaml

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -10,9 +10,25 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI must be defined");
   }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined");
+  }
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
   
   try {
-    await natsWrapper.connect("ticketing", "abc", "http://nats-srv:4222");
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
     // Event listener for when the connection is closed, logging a message and exiting the process
     natsWrapper.client.on("close", () => {
       console.log("NATS connection closed");
