0dfd43b79466ee6b79c4a73ce201d981da6ad1ac
guille garcia <ggarciacardiel@gmail.com>
2024-10-26 17:50:55 +0200

    update

--- common/src/events/ticket-updated-event.ts
+++ common/src/events/ticket-updated-event.ts
@@ -4,10 +4,10 @@ export interface TicketUpdatedEvent {
   subject: Subjects.TicketUpdated;
   data: {
     id: string;
+    version: number;
     title: string;
     price: number;
     userId: string;
-    version: number;
   };
 }
 
