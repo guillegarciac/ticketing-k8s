79fa487e96f4850a63d611e9e8cebaceaed92ff3
guille garcia <ggarciacardiel@gmail.com>
2024-10-26 18:03:14 +0200

    includes version in listener

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -40,10 +40,10 @@ router.put(
     await ticket.save();
     new TicketUpdatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
+      version: ticket.version,
       title: ticket.title,
       price: ticket.price,
       userId: ticket.userId,
-      version: ticket.version,
     });
 
     res.send(ticket);
