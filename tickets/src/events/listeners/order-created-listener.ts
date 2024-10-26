1d1500254a681b91fa1f961a1c1451a51e6bfb04
guille garcia <ggarciacardiel@gmail.com>
2024-10-26 19:14:31 +0200

    adds orderId to ticket model to lock ticket edition while order in progress

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,14 +1,27 @@
 import { Listener, OrderCreatedEvent, Subjects } from '@ggctickets/common';
 import { queueGroupName } from './queue-group-name';
 import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../models/ticket';
 
 export class TicketCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-    console.log('Event data!', data);
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    // Find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
 
+    // If no ticket, throw error
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+    // Mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+
+    // Save the ticket
+    await ticket.save();
+
+    // Ack the message
     msg.ack();
   }
 }
