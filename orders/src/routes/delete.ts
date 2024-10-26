9c7e08716774e432ab2309a32aa3214f277bf615
guille garcia <ggarciacardiel@gmail.com>
2024-10-26 13:42:34 +0200

    updates delete order route and errors

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,23 @@
 import express, { Request, Response } from 'express';
+import { requireAuth, NotAuthorizedError, NotFoundError } from '@ggctickets/common';
+import { Order, OrderStatus } from '../models/order';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send('Orders delete OK');
+router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+  const { orderId } = req.params;
+  
+  const order = await Order.findById(orderId);
+  
+  if (!order) {
+    throw new NotFoundError();
+  }
+  if (order.userId !== req.currentUser!.id) {
+  throw new NotAuthorizedError();
+  }
+  order.status = OrderStatus.Cancelled;
+  await order.save();
+  res.status(204).send(order);
 });
 
 export { router as deleteOrderRouter };
