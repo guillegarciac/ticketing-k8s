9e716c46c84153afb79522703122d355d5921a86
guille garcia <ggarciacardiel@gmail.com>
2024-10-26 16:50:02 +0200

    update ticket model in orders to include id, creates listeners for ticket create and update

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -3,6 +3,7 @@ import { Order, OrderStatus } from "./order";
 
 
 interface TicketAttrs {
+  id: string;
   title: string;
   price: number;
 }
@@ -39,7 +40,11 @@ const ticketSchema = new mongoose.Schema(
 );
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
-  return new Ticket(attrs);
+  return new Ticket({
+    _id: attrs.id,
+    title: attrs.title,
+    price: attrs.price,
+  });
 };
 
   // Run query to look at all orders. Find an order where the ticket
