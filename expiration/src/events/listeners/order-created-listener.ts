81bb6c07a6e8b2614af8d6e3e3177feb84e9537c
guille garcia <ggarciacardiel@gmail.com>
2024-10-27 12:16:02 +0100

    updates exp order created listener to include epxirationQueue

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,14 +1,17 @@
 import { Listener, OrderCreatedEvent, Subjects } from '@ggctickets/common';
 import { queueGroupName } from './queue-group-name';
 import { Message } from 'node-nats-streaming';
+import { expirationQueue } from '../../queues/expiration-queue';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  onMessage(data: any, msg: Message) {
-    console.log('Event data!', data);
+async onMessage(data: any, msg: Message) {
+  await expirationQueue.add({
+    orderId: data.id,
+  });
 
-    msg.ack();
-  }
+  msg.ack();
+}
 }
