5fc57ed00a320f66c78b10a9b86bf4c4aa2fb9b4
guille garcia <ggarciacardiel@gmail.com>
2024-10-29 18:32:15 +0100

    creates ingress for payments-srv and new.ts route

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -3,6 +3,7 @@ import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from '@ggctickets/common';
+import { createChargeRouter } from "./routes/new";
 
 
 const app = express();
@@ -16,6 +17,8 @@ app.use(
 );
 
 app.use(currentUser);
+
+app.use(createChargeRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
