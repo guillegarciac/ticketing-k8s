4c5163c339049bb8260c1c82789ffb108bcee483
guille garcia <ggarciacardiel@gmail.com>
2024-10-29 18:58:35 +0100

    creates stripe.ts and create function in new.ts

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import { stripe } from '../stripe';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest, BadRequestError, NotFoundError, NotAuthorizedError, OrderStatus } from '@ggctickets/common';
 import { Order } from '../models/order';
@@ -25,6 +26,12 @@ router.post('/api/payments', requireAuth, [
     throw new BadRequestError('Cannot pay for a cancelled order');
   }
 
+  await stripe.charges.create({
+    currency: 'usd',
+    amount: order.price * 100,
+    source: token
+  });
+  
   res.send({ success: true });
 });
 
