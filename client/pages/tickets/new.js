5839847942271742e0fbc1eaed8b834413151c52
guille garcia <ggarciacardiel@gmail.com>
2024-11-02 15:17:34 +0100

    updates index.js to include List of Tickets

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Router from "next/router";
 import useRequest from "../../hooks/use-request";
 
 const NewTicket = () => {
@@ -12,14 +13,14 @@ const NewTicket = () => {
       title,
       price,
     },
-    onSuccess: (ticket) => console.log(ticket),
+    onSuccess: () => Router.push("/"),
   });
 
   const onSubmit = (event) => {
     event.preventDefault();
     doRequest();
   }
-  
+
   const onBlur = () => {
     const value = parseFloat(price);
     if (isNaN(value)) {
