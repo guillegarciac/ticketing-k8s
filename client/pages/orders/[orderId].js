78d96e447a303f7202998f0b11cde998cc2bf347
guille garcia <ggarciacardiel@gmail.com>
2024-11-02 15:31:06 +0100

    adds Router to navigate to orderId

---
+++ client/pages/orders/[orderId].js
@@ -0,0 +1,17 @@
+const OrderShow = ({ order }) => {
+  return (
+    <div>
+      <h1>Order Show</h1>
+    </div>
+  );
+}
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
+};
+
+export default OrderShow;
+
