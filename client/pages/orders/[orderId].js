ec6374cb6c52666df9730d7e2ebc5ddf24748591
guille garcia <ggarciacardiel@gmail.com>
2024-11-02 15:38:49 +0100

    adds expiration Timer to order

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,7 +1,25 @@
+import { useState, useEffect } from 'react';
+
 const OrderShow = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState('');
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [order]);
+
   return (
     <div>
-      <h1>Order Show</h1>
+      Time left to pay: {timeLeft} seconds
     </div>
   );
 }
