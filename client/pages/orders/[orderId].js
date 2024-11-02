736e5c0f9ba13d99ff54a7add92911195bf54943
guille garcia <ggarciacardiel@gmail.com>
2024-11-02 15:39:59 +0100

    adds timeLeft <0 to orderExpired

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 const OrderShow = ({ order }) => {
-  const [timeLeft, setTimeLeft] = useState('');
+  const [timeLeft, setTimeLeft] = useState(0);
 
   useEffect(() => {
     const findTimeLeft = () => {
@@ -17,6 +17,10 @@ const OrderShow = ({ order }) => {
     };
   }, [order]);
 
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
+  
   return (
     <div>
       Time left to pay: {timeLeft} seconds
